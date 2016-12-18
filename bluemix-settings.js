24cbcad10b56a6277a0fbfb826aaae5b8bdac47d
Yokesh Sankar <yokeshvijayakumar@gmail.com>
2016-12-18 05:47:05 -0600

    CRC - bluemix-settings.js and package.jason

--- bluemix-settings.js
+++ bluemix-settings.js
@@ -51,7 +51,9 @@ var settings = module.exports = {
     // Serve up the welcome page
     httpStatic: path.join(__dirname,"public"),
 
-    functionGlobalContext: { },
+    functionGlobalContext: {
+    	crc:require('crc')
+    },
 
     storageModule: require("./couchstorage")
 }
